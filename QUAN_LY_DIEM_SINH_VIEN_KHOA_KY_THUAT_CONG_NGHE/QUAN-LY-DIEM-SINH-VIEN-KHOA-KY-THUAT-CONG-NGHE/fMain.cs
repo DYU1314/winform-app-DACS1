7966c37ee275edc2004eaf6089b2f10289e2e36c
Le-Hoang-Duy <lhduy.dyu@gmail.com>
2022-04-06 12:14:34 +0700

    push project

---
+++ QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE/QUAN-LY-DIEM-SINH-VIEN-KHOA-KY-THUAT-CONG-NGHE/fMain.cs
@@ -0,0 +1,268 @@
+﻿using QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE.DAO;
+using QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE
+{
+    public partial class frmMain : Form
+    {
+        public static string TenTaiKhoan = "";
+        private string UserName = string.Empty;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            LoadForm();
+        }
+
+        #region MeThods
+
+        private void LoadForm()
+        {
+            mngQLDSV.Visible = false;
+            tmgDangXuat.Visible = false;
+            btntmgThoat.Visible = false;
+            LoadQuyen();
+        }
+
+        private void LoadQuyen()
+        {
+            if (UserName != "")
+            {
+                TaiKhoan user = TaiKhoanDAO.Instance.GetTaiKhoanByTenTaiKhoan(UserName);
+                if (user.Idquyen < 0 && user.Idquyen > 4)
+                {
+                    tmgQuanLy.Visible = false;
+                    Application.Exit();
+                }
+                else
+                {
+                    if (user.Tenhienthi != "")
+                    {
+                        tmgTenHienThi.Text = user.Tenhienthi;
+                    }
+                    else
+                    {
+                        tmgTenHienThi.Text = "Thông tin";
+                    }
+
+                    if (user.Idquyen == 2)
+                    {
+                        tmgQuanLy.Visible = false;
+                        tmgSinhVien.Visible = false;
+                        tmgTaiKhoan.Visible = true;
+                    }
+                    else
+                    {
+                        if (user.Idquyen == 3)
+                        {
+                            tmgQuanLy.Visible = true;
+                            tmgSinhVien.Visible = true;
+                            tmgTaiKhoan.Visible = true;
+                        }
+                        else
+                        {
+                            if (user.Idquyen == 4)
+                            {
+                                tmgQuanLy.Visible = false;
+                                tmgSinhVien.Visible = true;
+                                tmgTaiKhoan.Visible = true;
+                            }
+                        }
+                    }
+                    mngQLDSV.Visible = true;
+                    tmgDangXuat.Visible = true;
+                    btntmgThoat.Visible = true;
+                }
+            }
+        }
+
+        private bool DangNhap(string userName, string passWord)
+        {
+            return TaiKhoanDAO.Instance.DangNhap(userName, passWord);
+        }
+
+        #endregion Methods
+
+        #region Event
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string userName = txtTenTaiKhoan.Text;
+            string passWord = txtMatKhau.Text;
+
+            if (userName == "" || passWord == "")
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu trống!!!\nVui lòng kiểm tra lại", "Lỗi đăng nhập", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtTenTaiKhoan.Clear();
+                txtMatKhau.Clear();
+                txtTenTaiKhoan.Focus();
+            }
+            else
+            {
+                if (DangNhap(userName, passWord))
+                {
+                    TaiKhoan user = TaiKhoanDAO.Instance.GetTaiKhoanByTenTaiKhoan(userName);
+                    TenTaiKhoan = user.Tentaikhoan;
+                    UserName = user.Tentaikhoan;
+                    pnlLogin.Visible = false;
+                    mngQLDSV.Visible = true;
+                    tmgDangXuat.Visible = true;
+                    btntmgThoat.Visible = true;
+                    if (user.Idquyen < 0 && user.Idquyen > 4)
+                    {
+                        tmgQuanLy.Visible = false;
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        if (user.Tenhienthi != "")
+                        {
+                            tmgTenHienThi.Text = user.Tenhienthi;
+                        }
+                        else
+                        {
+                            tmgTenHienThi.Text = "Thông tin";
+                        }
+
+                        if (user.Idquyen == 1)
+                        {
+                            tmgQuanLy.Visible = true;
+                            tmgSinhVien.Visible = true;
+                            tmgTaiKhoan.Visible = true;
+                        }
+                        else
+                        {
+                            if (user.Idquyen == 2)
+                            {
+                                tmgQuanLy.Visible = false;
+                                tmgSinhVien.Visible = false;
+                                tmgTaiKhoan.Visible = true;
+                            }
+                            else
+                            {
+                                if (user.Idquyen == 3)
+                                {
+                                    tmgQuanLy.Visible = true;
+                                    tmgSinhVien.Visible = true;
+                                    tmgTaiKhoan.Visible = true;
+                                }
+                                else
+                                {
+                                    if (user.Idquyen == 4)
+                                    {
+                                        tmgQuanLy.Visible = false;
+                                        tmgSinhVien.Visible = true;
+                                        tmgTaiKhoan.Visible = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Sai tên tài khoản hoặc mật khẩu!!!\nVui lòng kiểm tra lại", "Lỗi đăng nhập", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtTenTaiKhoan.Clear();
+                    txtMatKhau.Clear();
+                    txtTenTaiKhoan.Focus();
+                }
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn muốn thoát khỏi chương trình???", "thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void tmgDangXuat_Click(object sender, EventArgs e)
+        {
+            TenTaiKhoan = "";
+            UserName = "";
+            LoadForm();
+            pnlLogin.Visible = true;
+            txtMatKhau.Clear();
+            txtTenTaiKhoan.Focus();
+            TenTaiKhoan = "";
+        }
+
+        private void tmgTenHienThi_Click(object sender, EventArgs e)
+        {
+            frmTaiKhoan f = new frmTaiKhoan();
+            mngQLDSV.Enabled = false;
+            f.ShowDialog();
+            LoadForm();
+            mngQLDSV.Enabled = true;
+        }
+
+        private void tmgQuanLy_Click(object sender, EventArgs e)
+        {
+            frmQuanLy f = new frmQuanLy();
+            mngQLDSV.Enabled = false;
+            f.ShowDialog();
+            mngQLDSV.Enabled = true;
+        }
+
+        private void lblThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn muốn thoát khỏi chương trình???", "thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btntmgThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn muốn thoát khỏi chương trình???", "thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void pnlLogin_ParentChanged(object sender, EventArgs e)
+        {
+            if (pnlLogin.Visible == false)
+            {
+                this.CancelButton = btntmgThoat;
+                return;
+            }
+            else
+            {
+                this.CancelButton = btnThoat;
+            }
+        }
+
+        #endregion Event
+
+        private void tmgSinhVien_Click(object sender, EventArgs e)
+        {
+            frmQuanLyDiem f = new frmQuanLyDiem();
+            mngQLDSV.Enabled = false;
+            f.ShowDialog();
+            mngQLDSV.Enabled = true;
+        }
+    }
+}
---
+++ QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE/QUAN-LY-DIEM-SINH-VIEN-KHOA-KY-THUAT-CONG-NGHE/fReport.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE
+{
+    partial class frmReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource3 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.USP_LoadDanhSachSinhVienReportBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.db_QLDSVKKTCNDataSet = new QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE.db_QLDSVKKTCNDataSet();
+            this.USP_DanhSachSinhVienVaDiemHocPhanReportBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ReportNe = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.USP_LoadDanhSachSinhVienReportTableAdapter = new QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE.db_QLDSVKKTCNDataSetTableAdapters.USP_LoadDanhSachSinhVienReportTableAdapter();
+            this.USP_DanhSachSinhVienVaDiemHocPhanReportTableAdapter = new QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE.db_QLDSVKKTCNDataSetTableAdapters.USP_DanhSachSinhVienVaDiemHocPhanReportTableAdapter();
+            this.USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopTableAdapter = new QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE.db_QLDSVKKTCNDataSetTableAdapters.USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopTableAdapter();
+            this.ReportDay = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.ReportNuaNe = new Microsoft.Reporting.WinForms.ReportViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.USP_LoadDanhSachSinhVienReportBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_QLDSVKKTCNDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.USP_DanhSachSinhVienVaDiemHocPhanReportBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // USP_LoadDanhSachSinhVienReportBindingSource
+            // 
+            this.USP_LoadDanhSachSinhVienReportBindingSource.DataMember = "USP_LoadDanhSachSinhVienReport";
+            this.USP_LoadDanhSachSinhVienReportBindingSource.DataSource = this.db_QLDSVKKTCNDataSet;
+            // 
+            // db_QLDSVKKTCNDataSet
+            // 
+            this.db_QLDSVKKTCNDataSet.DataSetName = "db_QLDSVKKTCNDataSet";
+            this.db_QLDSVKKTCNDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // USP_DanhSachSinhVienVaDiemHocPhanReportBindingSource
+            // 
+            this.USP_DanhSachSinhVienVaDiemHocPhanReportBindingSource.DataMember = "USP_DanhSachSinhVienVaDiemHocPhanReport";
+            this.USP_DanhSachSinhVienVaDiemHocPhanReportBindingSource.DataSource = this.db_QLDSVKKTCNDataSet;
+            // 
+            // USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopBindingSource
+            // 
+            this.USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopBindingSource.DataMember = "USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLop";
+            this.USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopBindingSource.DataSource = this.db_QLDSVKKTCNDataSet;
+            // 
+            // ReportNe
+            // 
+            this.ReportNe.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.USP_LoadDanhSachSinhVienReportBindingSource;
+            this.ReportNe.LocalReport.DataSources.Add(reportDataSource1);
+            this.ReportNe.LocalReport.ReportEmbeddedResource = "QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE.Report1.rdlc";
+            this.ReportNe.Location = new System.Drawing.Point(130, 0);
+            this.ReportNe.Name = "ReportNe";
+            this.ReportNe.Size = new System.Drawing.Size(1058, 815);
+            this.ReportNe.TabIndex = 0;
+            // 
+            // USP_LoadDanhSachSinhVienReportTableAdapter
+            // 
+            this.USP_LoadDanhSachSinhVienReportTableAdapter.ClearBeforeFill = true;
+            // 
+            // USP_DanhSachSinhVienVaDiemHocPhanReportTableAdapter
+            // 
+            this.USP_DanhSachSinhVienVaDiemHocPhanReportTableAdapter.ClearBeforeFill = true;
+            // 
+            // USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopTableAdapter
+            // 
+            this.USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopTableAdapter.ClearBeforeFill = true;
+            // 
+            // ReportDay
+            // 
+            this.ReportDay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            reportDataSource2.Name = "DataSet1";
+            reportDataSource2.Value = this.USP_DanhSachSinhVienVaDiemHocPhanReportBindingSource;
+            this.ReportDay.LocalReport.DataSources.Add(reportDataSource2);
+            this.ReportDay.LocalReport.ReportEmbeddedResource = "QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE.Report2.rdlc";
+            this.ReportDay.Location = new System.Drawing.Point(130, 0);
+            this.ReportDay.Name = "ReportDay";
+            this.ReportDay.Size = new System.Drawing.Size(1058, 815);
+            this.ReportDay.TabIndex = 1;
+            // 
+            // ReportNuaNe
+            // 
+            this.ReportNuaNe.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            reportDataSource3.Name = "DataSet1";
+            reportDataSource3.Value = this.USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopBindingSource;
+            this.ReportNuaNe.LocalReport.DataSources.Add(reportDataSource3);
+            this.ReportNuaNe.LocalReport.ReportEmbeddedResource = "QUAN_LY_DIEM_SINH_VIEN_KHOA_KY_THUAT_CONG_NGHE.Report3.rdlc";
+            this.ReportNuaNe.Location = new System.Drawing.Point(130, 0);
+            this.ReportNuaNe.Name = "ReportNuaNe";
+            this.ReportNuaNe.Size = new System.Drawing.Size(1058, 815);
+            this.ReportNuaNe.TabIndex = 2;
+            // 
+            // frmReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.ClientSize = new System.Drawing.Size(1327, 815);
+            this.Controls.Add(this.ReportNuaNe);
+            this.Controls.Add(this.ReportNe);
+            this.Controls.Add(this.ReportDay);
+            this.Name = "frmReport";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmReport_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.USP_LoadDanhSachSinhVienReportBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_QLDSVKKTCNDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.USP_DanhSachSinhVienVaDiemHocPhanReportBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer ReportNe;
+        private System.Windows.Forms.BindingSource USP_LoadDanhSachSinhVienReportBindingSource;
+        private db_QLDSVKKTCNDataSet db_QLDSVKKTCNDataSet;
+        private db_QLDSVKKTCNDataSetTableAdapters.USP_LoadDanhSachSinhVienReportTableAdapter USP_LoadDanhSachSinhVienReportTableAdapter;
+        private System.Windows.Forms.BindingSource USP_DanhSachSinhVienVaDiemHocPhanReportBindingSource;
+        private db_QLDSVKKTCNDataSetTableAdapters.USP_DanhSachSinhVienVaDiemHocPhanReportTableAdapter USP_DanhSachSinhVienVaDiemHocPhanReportTableAdapter;
+        private System.Windows.Forms.BindingSource USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopBindingSource;
+        private db_QLDSVKKTCNDataSetTableAdapters.USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopTableAdapter USP_DanhSachDiemTrungBinhHocKiCuaSinhVienTrongLopTableAdapter;
+        private Microsoft.Reporting.WinForms.ReportViewer ReportDay;
+        private Microsoft.Reporting.WinForms.ReportViewer ReportNuaNe;
+    }
+}
